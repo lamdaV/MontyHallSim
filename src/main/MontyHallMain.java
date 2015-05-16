196b407ba9a7ee8d0dea98167fb3ad2ff0fc44b5
lamdaV <dav_lam@ymail.com>
2015-05-16 01:51:11 -0400

    Initial Commit

---
+++ src/main/MontyHallMain.java
@@ -0,0 +1,116 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * The main class.
+ *
+ * @author lamd.
+ *         Created May 16, 2015.
+ */
+public class MontyHallMain {
+	/**
+	 * Simulates the Monty Hall problem.
+	 *
+	 * @param args
+	 */
+	@SuppressWarnings("boxing")
+	public static void main(String[] args) {
+		// Variables.
+		String userInput = null;
+		int wrongDoor;
+		boolean isCorrect;
+		double win = 0;
+		double lose = 0;
+		double probability;
+		int userChoice;
+		
+		// Setup.
+		Scanner sc = new Scanner(System.in);
+		MontyHallSimulator sim = new MontyHallSimulator(0);
+
+		while (true) {
+			// Print the options.
+			sim.printDoors();
+			
+			// Sets a random choice.
+			sim.setRandomChoice();
+
+			System.out
+					.println("Enter a number between 1 and 3 as choices. Enter 'q' to quit.");
+			userInput = sc.next();
+			System.out.println();
+			
+			// Quit.
+			if (userInput.equals("q")) {
+				break;
+			}
+			
+			// Catches erroneous entries.
+			try {
+				userChoice = Integer.parseInt(userInput);
+				if (userChoice <= 0 || userChoice > 3) {
+					System.out.println("Please enter a number between 1 - 3.");
+					continue;
+				}
+				sim.setUserChoice(Integer.parseInt(userInput));
+			} catch (NumberFormatException e) {
+				System.out
+						.println("Please enter a number between 1 - 3 or enter 'q' to quit!\n");
+				continue;
+			}
+			
+			// Give the user a hint by getting one of the two incorrect options.
+			wrongDoor = sim.getWrongDoor();
+			System.out.printf("One of the incorrect choices was door %d.\n",
+					wrongDoor);
+			sim.printDoors();
+			
+			// Ask if the user would like to switch his choice.
+			System.out.println("Do you wish to change your choice? (y/n)");
+			
+			// Ensures 'y' or 'n' is typed.
+			while (true) {
+				userInput = sc.next();
+				System.out.println();
+				
+				if (userInput.equals("y")) {
+					sim.switchDoor();
+					break;
+				}
+				
+				if (userInput.equals("n")) {
+					break;
+				}
+				
+				System.out.println("Please enter 'y' for yes and 'n' for no.");
+			}
+			
+			// Checks if the user has correctly decided.
+			isCorrect = sim.checkValidity();
+			
+			// Increment the results accordingly.
+			if (isCorrect) {
+				win++;
+			}
+			lose++;
+			
+			// Reset the simulator.
+			sim.resetSim();
+		}
+		
+		// Calculate the probability.
+		probability = win / (win + lose);
+		
+		// Print the results to console.
+		System.out.println("~~~~~~~Results~~~~~~~");
+		System.out.printf(" Win:\t\t%.2f", win);
+		System.out.printf("\n Lose:\t\t%.2f", lose);
+		System.out.printf("\n Probability:\t%.2f", probability);
+		
+		// Close the Scanner.
+		sc.close();
+
+	}
+
+}
---
+++ src/main/MontyHallSimulator.java
@@ -0,0 +1,134 @@
+package main;
+
+import java.util.Random;
+
+/**
+ * A simple simulator of the Monty Hall problem with 3 doors.
+ * 
+ * @author lamd. Created May 16, 2015.
+ */
+public class MontyHallSimulator {
+	private int randomChoice;
+	private int userChoice;
+	private int[] options;
+	private int givenWrongDoor;
+
+	/**
+	 * Constructs a Monty Hall Problem simulator with 3 doors.
+	 * 
+	 * @param userChoice
+	 */
+	public MontyHallSimulator(int userChoice) {
+		this.userChoice = userChoice;
+		this.options = new int[3];
+		this.givenWrongDoor = -1;
+		populateOptions();
+	}
+
+	/**
+	 * Resets the simulator for subsequent runs.
+	 *
+	 */
+	public void resetSim() {
+		this.givenWrongDoor = -1;
+	}
+	
+	/**
+	 * Sets the userChoice to the desired choice.
+	 *
+	 * @param userChoice
+	 */
+	public void setUserChoice(int userChoice) {
+		this.userChoice = userChoice;
+	}
+
+	/**
+	 * Prints the options onto the console.
+	 *
+	 */
+	public void printDoors() {
+		String[] printSelection = new String[3];
+
+		for (int i = 0; i < this.options.length; i++) {
+			if (i == this.givenWrongDoor) {
+				printSelection[i] = "X";
+			} else {
+				printSelection[i] = Integer.toString(this.options[i]);
+			}
+		}
+
+		System.out.printf("|\t%s\t|\t%s\t|\t%s\t|\n", printSelection[0],
+				printSelection[1], printSelection[2]);
+	}
+
+	private void populateOptions() {
+		for (int i = 1; i <= 3; i++) {
+			this.options[i - 1] = i;
+		}
+	}
+
+	/**
+	 * Sets what the correct door should be.
+	 * 
+	 */
+	public void setRandomChoice() {
+		this.randomChoice = new Random().nextInt(3) + 1;
+	}
+
+	/**
+	 * Returns one of the two incorrect choices. It invalidates it from the options array with a -1.
+	 * 
+	 * @return integer of an incorrect choice.
+	 */
+	public int getWrongDoor() {
+		setRandomChoice();
+		
+		int randomWrongChoice;
+		
+		while (true) {
+			randomWrongChoice = new Random().nextInt(this.options.length);
+			
+			if (this.options[randomWrongChoice] != this.randomChoice && this.options[randomWrongChoice] != this.userChoice) {
+				break;
+			}
+		}
+		
+		this.givenWrongDoor = randomWrongChoice;
+		
+		return this.options[randomWrongChoice];
+	}
+
+	/**
+	 * Switches the userChoice to an unselected option that has not been given away.
+	 * 
+	 */
+	public void switchDoor() {
+		for (int i = 0; i < this.options.length; i++) {
+			if (i == this.givenWrongDoor) {
+				continue;
+			}
+			if (this.options[i] == this.userChoice) {
+				continue;
+			}
+			this.userChoice = this.options[i];
+			break;
+		}
+	}
+
+	/**
+	 * Checks if the user was correct.
+	 *
+	 * @return the boolean that corresponds with the user's correctness.
+	 */
+	public boolean checkValidity() {
+		if (this.userChoice == this.randomChoice) {
+			System.out.println("Congrats! You Win!");
+			return true;
+		}
+		System.out.println("You Lose!");
+		return false;
+	}
+	
+	
+
+}
